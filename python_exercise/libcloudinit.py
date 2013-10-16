58d100810d6fc6741caceaf7ee34d434683c4e23
vagrant <vagrant@precise64.(none)>
2013-10-16 00:49:09 +0000

    dsdfsf

---
+++ python_exercise/libcloudinit.py
@@ -0,0 +1,14 @@
+import os
+import re
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver
+
+Driver = get_driver(Provider.EC2)
+
+AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', 'AKIAJOM4IDJVZSUA4SEQ')
+AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', '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')
+assert AWS_ACCESS_KEY is not None
+assert AWS_SECRET_KEY is not None
+
+conn = Driver(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+
