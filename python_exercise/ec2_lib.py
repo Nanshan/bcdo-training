47205985d1ad2e648c8093a1528b4e8c4137363d
vagrant <vagrant@precise64.(none)>
2013-10-16 05:31:24 +0000

    sd

--- python_exercise/ec2_lib.py
+++ python_exercise/ec2_lib.py
@@ -1,39 +1,50 @@
-#!/usr/bin/env python
-
-import os
-import re
+#!/usr/bin/python
 import sys
 import argparse
-from libcloud.compute.types import Provider
-from libcloud.compute.providers import get_driver
+from libcloudinit import *
+class ec2run(object):
+    def __inti__(self):
+        pass 
+      
+    def parse_args(self, args):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-r", "--region", help="ec2 region")
+        parser.add_argument("-z", "--availability_zone", help="ec2 availability zone")
+        parser.add_argument("-a", "--ami", help="AMI name")
+        parser.add_argument("-d", "--user-data", help="User data string")
+        parser.add_argument("-f", "--user_data_file", help="Read user data from file")
+        parser.add_argument("-g", "--group", help="Ec2 security group name/id")
+        parser.add_argument("-k", "--key", help="Ec2 Keypair name")
+        parser.add_argument("-n", "--instance-count", help="Maximum instances to launch")
+        parser.add_argument("-v", "--verbose", help="Verbose Output", action="store_true", default="False")
+        parser.add_argument("-t", "--instance_type", help="Ec2 Instance Type", default="t1.micro")
+        self.args=parser.parse_args(args)
+        return self.args
 
-Driver = get_driver(Provider.EC2)
+    def get_arg(self, arg):
+        return self.args.__getattribute__(arg)
 
-AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', None)
-AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', None)
-assert AWS_ACCESS_KEY is not None
-assert AWS_SECRET_KEY is not None
-conn = Driver(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    def print_args(self,args):
+        arg_list=vars(args)
+        for key in arg_list:
+            print "%s: %s" % (key, self.get_arg(key))
+ 
+    def validate_image(self,arg):
+        try:       
+           image_list=conn.list_images(ex_image_ids=[ self.args.ami ])
+           print "Name: %s" % image.name
+           print "Id: %s" % image.id
+        except:
+           print "No image"
+        #for item in image_list:
+         #   print item        
 
-parser = argparse.ArgumentParser()
+def main():
+  ec2obj = ec2run()
+  args=ec2obj.parse_args(sys.argv[1:])
+  print args
+  ec2obj.print_args(args)
+  print ec2obj.validate_image('ami')
 
-parser.add_argument("-r", "--region", help="ec2 region")
-
-parser.add_argument("-z", "--availability_zone", help="ec2 availability zone")
-parser.add_argument("-a", "--ami", help="AMI name")
-parser.add_argument("-d", "--user-data", help="User data string")
-parser.add_argument("-f", "--user_data_file", help="Read user data from file")
-parser.add_argument("-g", "--group", help="Ec2 security group name/id")
-parser.add_argument("-k", "--key", help="Ec2 Keypair name")
-parser.add_argument("-n", "--instance-count", help="Maximum instances to launch")
-parser.add_argument("-v", "--verbose", help="Verbose Output", action="store_true", default="False")
-parser.add_argument("-t", "--instance_type", help="Ec2 Instance Type", default="t1.micro")
-args = parser.parse_args()
-
-dic_args=vars(args)
-
-for key in dic_args.keys():
-    print "%s: %s" % (key, dic_args[key])
-
-def get_arg(self,arg):
-    return eval('self.args.%s' % arg)
+if __name__ == '__main__':
+    main()
