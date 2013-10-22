5cca2d83b68b9fb8767309dd8f3cc16fdf559489
Leah <lifang.moler@gmail.com>
2013-10-21 23:01:47 -0700

    update ec2run.py

--- python_exercise/ec2run.py
+++ python_exercise/ec2run.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python
 import sys
 import argparse
-<<<<<<< HEAD
-parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--region", help="ec2 region")
-parser.add_argument("-z", "--availability_zone", help="ec2 availability zone")
-parser.add_argument("-a", "--ami", help="AMI name")
-parser.add_argument("-d", "--user-data", help="User data string")
-parser.add_argument("-f", "--user_data_file", help="Read user data from file")
-parser.add_argument("-g", "--group", help="Ec2 security group name/id")
-parser.add_argument("-k", "--key", help="Ec2 Keypair name")
-parser.add_argument("-n", "--instance-count", help="Maximum instances to launch")
-parser.add_argument("-v", "--verbose", help="Verbose Output", action="store_true", default="False")
-parser.add_argument("-t", "--instance_type", help="Ec2 Instance Type", default="t1.micro")
-args = parser.parse_args()
+from libcloudinit import conn
 
-
-dic_args=vars(args)
-
-for key in dic_args.keys():
-    print "%s: %s" % (key, dic_args[key])
-
-
-
-=======
 class ec2run(object):
     def __inti__(self):
         pass 
@@ -51,6 +30,16 @@ class ec2run(object):
         for key in arg_list:
             print "%s: %s" % (key, self.get_arg(key))
 
+    def validate_image(self, ami):
+        try:
+			    #           image=conn.list_images(ex_image_ids=[ self.args.ami ])
+           image=conn.list_images(ex_image_ids=[ ami ])[0]
+           print image
+						                #print "Name: %s" % image.name
+	   return 1
+        except:
+	   return 0
+
 
 def main():
   ec2obj = ec2run()
@@ -59,4 +48,3 @@ def main():
 
 if __name__ == '__main__':
     main()
->>>>>>> 3cbc965372529a8d6affcc51d1e0ef66216748ab
