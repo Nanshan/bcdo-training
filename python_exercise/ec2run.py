1f78f7883de964fe1952ed1631fd1b1eaa5f748a
Ubuntu <ubuntu@ip-172-31-15-2.us-west-1.compute.internal>
2013-10-23 19:41:12 +0000

    ec2run 3

--- python_exercise/ec2run.py
+++ python_exercise/ec2run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 import argparse
-from libcloudinit import conn
+from libcloudinit import conn,region
 
 class ec2run(object):
     def __inti__(self):
@@ -16,9 +16,12 @@ class ec2run(object):
         parser.add_argument("-f", "--user_data_file", help="Read user data from file")
         parser.add_argument("-g", "--group", help="Ec2 security group name/id")
         parser.add_argument("-k", "--key", help="Ec2 Keypair name")
-        parser.add_argument("-n", "--instance-count", help="Maximum instances to launch")
+        parser.add_argument("-n", "--instance-count", help="Maximum instances to launch", default=1)
         parser.add_argument("-v", "--verbose", help="Verbose Output", action="store_true", default="False")
         parser.add_argument("-t", "--instance_type", help="Ec2 Instance Type", default="t1.micro")
+        parser.add_argument("--role", help="roles defined in")
+        parser.add_argument("-s", "--stack", help="")
+        parser.add_argument("-b", "--branch", help="git branch")
         self.args=parser.parse_args(args)
         return self.args
 
@@ -32,19 +35,29 @@ class ec2run(object):
 
     def validate_image(self, ami):
         try:
-			    #           image=conn.list_images(ex_image_ids=[ self.args.ami ])
+           #image=conn.list_images(ex_image_ids=[ self.args.ami ])
            image=conn.list_images(ex_image_ids=[ ami ])[0]
            print image
-						                #print "Name: %s" % image.name
+           #print "Name: %s" % image.name
 	   return 1
         except:
 	   return 0
-
+    
+    def create_ec2_instance(self, args):
+        arg_list=vars(self.args)
+  	kwargs = { 'image': args.ami,
+             	   'size': args.instance_type,
+                  
+		 }
+        return conn.create_node(kwargs)
 
 def main():
   ec2obj = ec2run()
+  libobj = libcloudinit('EC2_US_WEST')
   args=ec2obj.parse_args(sys.argv[1:])
   ec2obj.print_args(args)
+  print  libobj.region    
+  ec2obj.create_ec2_instance(args)
 
 if __name__ == '__main__':
     main()
--- python_exercise/libcloudinit.py
+++ python_exercise/libcloudinit.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python
 import os
 import re
 import libcloud.security
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
+class ec2conn(object):
+    def _init_(self, region):
+        self.region= region
+     
+    ec2_map = { 'us-east-1': Provider.EC2_US_EAST,
+                'us-west-2': Provider.EC2_US_WEST }
 
-Driver = get_driver(Provider.EC2_US_WEST)
-
-AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', 'AKIAJAYGQMYOTCOULBYQ')
-AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', '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')
-assert AWS_ACCESS_KEY is not None
-assert AWS_SECRET_KEY is not None
-
-conn = Driver(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    Driver = get_driver(ec2_map[self.region])
+    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY', 'AKIAJAYGQMYOTCOULBYQ')
+    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY', '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')
+    assert AWS_ACCESS_KEY is not None
+    assert AWS_SECRET_KEY is not None
+    conn = Driver(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 
