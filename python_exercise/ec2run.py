8eb03ee4c6aa0eeaf08f7fa80e33d4b4f0241639
Leah <lifang.moler@gmail.com>
2013-11-04 08:19:37 -0800

    ec2run

--- python_exercise/ec2run.py
+++ python_exercise/ec2run.py
@@ -8,28 +8,28 @@ import os
 class ec2run(object):
 
     def __init__(self):
-        self.ec2 = ec2conn('us-west-1') 
+        self.ec2 = ec2conn('us-west-1')
 
     def parse_args(self, args):
         parser.add_argument("-r", "--region", help="ec2 region")
-        parser.add_argument("-z", "--availability_zone", 
-			    help="ec2 availability zone")
+        parser.add_argument("-z", "--availability_zone",
+                            help="ec2 availability zone")
         parser = argparse.ArgumentParser()
        # parser.add_argument("-r", "--region", help="ec2 region")
         parser.add_argument("-z", "--availability_zone",
-			    help="ec2 availability zone")
+                            help="ec2 availability zone")
         parser.add_argument("-a", "--ami", help="AMI name")
         parser.add_argument("-d", "--user-data", help="User data string")
         parser.add_argument("-f", "--user_data_file",
-			    help="Read user data from file")
+                            help="Read user data from file")
         parser.add_argument("-g", "--group", help="Ec2 security group name/id")
         parser.add_argument("-k", "--key", help="Ec2 Keypair name")
         parser.add_argument("-n", "--instance-count",
-			    help="Maximum instances to launch", default=1)
+                            help="Maximum instances to launch", default=1)
         parser.add_argument("-v", "--verbose", help="Verbose Output",
-			    action="store_true", default="False")
+                            action="store_true", default="False")
         parser.add_argument("-t", "--instance_type",
-			    help="Ec2 Instance Type", default="t1.micro")
+                            help="Ec2 Instance Type", default="t1.micro")
         parser.add_argument('-r', "--role", help="roles defined in")
         parser.add_argument("-s", "--stack", help="")
         parser.add_argument("-b", "--branch", help="git branch")
@@ -49,13 +49,13 @@ class ec2run(object):
         try:
             self._image = self.ec2.conn.list_images(ex_image_ids=[ami])[0]
             print self._image
-	    return 1
+            return 1
         except:
-	    return 0
+            return 0
 
     def validate_size(self, size):
-        try: 
-            self._size = self.ec2.conn.list_sizes()[0]    
+        try:
+            self._size = self.ec2.conn.list_sizes()[0]
             print self._size
             return 1
         except:
@@ -63,11 +63,11 @@ class ec2run(object):
 
     def create_ec2_instance(self, args):
         arg_list = vars(self.args)
-  	kwargs = {'name': args.name,
-                  'image': self._image, 
-		  'size': self._size,
+        kwargs = {'name': args.name,
+                  'image': self._image,
+                  'size': self._size,
                   'ex_keyname': args.key,
-                  'ex_securitygroup': args.group}
+                  'ex_securitygroup': args.group, }
         return self.ec2.conn.create_node(**kwargs)
 
     def create_grains(self, filename, mode, args):
